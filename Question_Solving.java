febb27acbd65af34a004aadb1829af538830b497
Holy-Knight <vnycall74@naver.com>
2017-03-16 17:50:10 +0900

    과목없애고 진짜 시험으로 변경
    과목없애고 오로지 문재 갯수, 문제당 점수, 정답만 입력

--- Question_Solving.java
+++ Question_Solving.java
@@ -4,73 +4,65 @@ public class Question_Solving
 {
    static Scanner sc = new Scanner(System.in);
 
-   static int question[] = new int[90];
-   static int answer[] = new int[90];
+   static int question[] = new int[130];
+   static int answer[] = new int[130];
 
-   static double score1 = 0, score2 = 0;
-   static double result = 0;
+   static int answerCount, max;
+
+   static double score = 0;
+   static double answerScore = 0;
 
    public static void main(String[] args)
    {
-      output("개발자 : 유광무\n\n쓸모없는 리눅스 마스터 2급 2차 문제 풀이\n\n1과목 시작\n");
+      output("간단한 시험 테스트\n\n1. 문제 갯수 입력\n2. 문제 당 점수 : \n3. 문제푼 답 입력\n4. 정답 입력\n5. 결과 발표\n\n");
 
-      for(int i=1; i<=48; i++)
+      output("1 : 시험볼 문제의 갯수를 입력하세요 : ");
+      max = input();
+
+      output("2 : 문제 당 점수를 입력하세요 : ");
+      answerScore = inputD();
+
+      output("3 : 푼 문제의 답을 입력하세요.\n");
+
+      for(int i=1; i<=max; i++)
       {
          output(i + "번 : ");
          question[i] = input();
       }
 
-     output("\n\n2과목 시작\n");
+      output("\n\n시험 완료\n\n정답 입력\n\n");
 
-     for(int i=49; i<=80; i++)
-     {
-        output(i + "번 : ");
-        question[i] = input();
-     }
+      for(int i=1; i<=max; i++)
+      {
+         output(i + "번 : ");
+         answer[i] = input();
 
-     output("\n\n시험 완료\n\n정답 입력\n\n");
+         if(question[i] == answer[i])
+         {
+            score += answerScore;
+            answerCount += 1;
+         }
+      }
 
-     for(int i=1; i<=48; i++)
-     {
-        output(i + "번 : ");
-        answer[i] = input();
+      output("\n정답 입력 완료\n\n채점 결과\n\n");
 
-        if(question[i] == answer[i])
-        {
-           score1 += 1.5;
-        }
-     }
+      for(int i=1; i<=max; i++)
+      {
+         output(i + "번 : " + question[i] + " / " + answer[i]);
+      }
 
-     for(int i=49; i<=80; i++)
-     {
-        output(i + "번 : ");
-        answer[i] = input();
-
-        if(question[i] == answer[i])
-        {
-           score2 += 1.5;
-        }
-     }
-
-     output("\n정답 입력 완료\n\n채점 결과\n\n");
-
-     for(int i=1; i<=80; i++)
-     {
-        output(i + "번 : " + question[i] + " / " + answer[i]);
-     }
-
-     result = score1 + score2;
-
-     if(score1 + score2 >= 80.0)
-        output("\n\n합격!\n\n1과목 : " + score1 + "점\n2과목 : " + score2 + "\n\n최종 : " + result + "점");
-     else
-        output("\n\n불합격!\n\n1과목 : " + score1 + "점\n2과목 : " + score2 + "\n\n최종 : " + result + "점\n\n합격을 위해서는 " + (60.0 - result) / 1.25 + "문제를 추가로 맞추셨어야 합니다.");
+      output("당신은 " + max + "문제중 " + answerCount + "문제를 맞추셨습니다.\n점수는 " + score + "입니다.");
 
    }
 
    public static int input()
    {
       return sc.nextInt();
+   }
+
+   public static double inputD()
+   {
+      return sc.nextDouble();
    }
 
    public static void output(String text)
